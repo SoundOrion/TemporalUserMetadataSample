4181cd250dcd1afe9270f0dca8134d6db9af3618
hatsuyama <hatsuyama>
2026-04-25 17:00:16 +0900

    add

--- TemporalUserMetadataConsole/TemporalUserMetadataReader.cs
+++ TemporalUserMetadataConsole/TemporalUserMetadataReader.cs
@@ -34,6 +34,43 @@ public static class TemporalUserMetadataReader
         );
     }
 
+    public static async Task<(string? Summary, string? Details, Dictionary<string, string?> Memo)> GetAsync2(
+        ITemporalClient client,
+        string workflowId,
+        string? runId = null)
+    {
+        var res =
+            await client.Connection.WorkflowService
+                .DescribeWorkflowExecutionAsync(
+                    new DescribeWorkflowExecutionRequest
+                    {
+                        Namespace = client.Options.Namespace,
+                        Execution = new Temporalio.Api.Common.V1.WorkflowExecution
+                        {
+                            WorkflowId = workflowId,
+                            RunId = runId ?? ""
+                        }
+                    });
+
+        var meta = res.ExecutionConfig?.UserMetadata;
+
+        var memo = new Dictionary<string, string?>();
+
+        if (res.WorkflowExecutionInfo?.Memo?.Fields != null)
+        {
+            foreach (var field in res.WorkflowExecutionInfo.Memo.Fields)
+            {
+                memo[field.Key] = Decode(field.Value);
+            }
+        }
+
+        return (
+            Decode(meta?.Summary),
+            Decode(meta?.Details),
+            memo
+        );
+    }
+
     private static string? Decode(Payload? payload)
     {
         if (payload == null || payload.Data.IsEmpty)
