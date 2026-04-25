334a10e4abbee0c41c9d3025dd58048512f3beaf
hatsuyama <hatsuyama>
2026-04-25 17:42:38 +0900

    add

--- TemporalUserMetadataConsole/Program.cs
+++ TemporalUserMetadataConsole/Program.cs
@@ -9,7 +9,6 @@ var result = await TemporalUserMetadataReader.GetAsync(client, workflowId);
 Console.WriteLine(result.Summary);
 Console.WriteLine(result.Details);
 
-
 var handle = client.GetWorkflowHandle(workflowId);
 var desc = await handle.DescribeAsync();
 
@@ -23,6 +22,19 @@ foreach (var item in desc.Memo)
     };
 
     Console.WriteLine($"{item.Key}: {value}");
+}
+
+/////////////////////////////////////
+
+var result2 = await TemporalUserMetadataReader.GetAsync2(client, workflowId);
+
+Console.WriteLine($"Summary: {result2.Summary}");
+Console.WriteLine($"Details: {result2.Details}");
+
+Console.WriteLine("Memo:");
+foreach (var item in result2.Memo)
+{
+    Console.WriteLine($"{item.Key}: {item.Value}");
 }
 
 Console.ReadLine();
